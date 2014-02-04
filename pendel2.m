3c3bb2209e72c492382d8ca26ae2331c1fff5a3c
Ingelhag <hannesingelhag@hotmail.com>
2014-02-04 17:55:13 +0100

    Tisdag Version 1.03
    Kollsion funkar lite bättre, fuckar lite med marken men kom just på
    varje så ska bara flytta över det till DEN STORA datorn så kirrar ja
    biffen

--- pendel2.m
+++ pendel2.m
@@ -11,7 +11,7 @@ wWidth = 0.1;
 
 
 %Materials
-radius = [0.05 0.05];
+radius = [0.1 0.051];
 volume = [4*pi*(radius(1)^3)/3 4*pi*(radius(2)^3)/3];
 area = [4*pi*radius(1)^2 4*pi*radius(2)^2];
 density = [11340 11340];
@@ -26,11 +26,10 @@ circle_vec = 0:0.01:2*pi;
 air_constant = [0.47 0.47];
 g = 9.82;
 tStep = 0.01;
-CRestitution =0.6;
 
 % Initial angle = pi/4 & initial velocity & acceleration= 0
 theta_zero = [-(pi/2) (pi/2)];
-w_zero = [25 -25];
+w_zero = [30 -33];
 a_zero = [0 0];
 
 %% Pendulum
@@ -42,8 +41,11 @@ figure;
 %Calculating and drawing the pendulum
 for i=1:100
     clf;
+    
+    %Calc air-resistance
     airres = [(0.5*(velocity(1)^2)*area(1)*air_constant(1))/mass(1) (0.5*(velocity(2)^2)*area(2)*air_constant(2))/mass(2)];
     
+    %Direction of air-resistance depends on velvector
     if(velocity(1) < 0)
         airres(1) = airres(1)*-1;
     end
@@ -51,6 +53,7 @@ for i=1:100
         airres(2) = airres(2)*-1;
     end
     
+    %Calc Acc/Vel/Pos
     acceleration = [-(g/rope_length(1))*sin(theta(1)), -(g/rope_length(2))*sin(theta(2))];
     velocity = [euler(tStep, velocity(1), acceleration(1))-airres(1), euler(tStep, velocity(2), acceleration(2))- airres(2)];
     theta = [euler(tStep, theta(1), velocity(1)) euler(tStep, theta(2), velocity(2))];
@@ -71,7 +74,7 @@ for i=1:100
     yp = radius(1)*sin(circle_vec);
     yp2 = radius(2)*sin(circle_vec);
     patch(x(1)+xp,y(1)+yp, [1,0,0]);
-    patch(x(2)+xp2,y(2)+yp2, [1,0,0]);
+    patch(x(2)+xp2,y(2)+yp2, [0,1,0]);
     
     %Drawing the Wall
     wallXData=[wSize(1), wSize(1)+wWidth, wSize(1)+wWidth, wSize(2)-wWidth, wSize(2)-wWidth, wSize(2), wSize(2), wSize(1)];
@@ -89,6 +92,7 @@ end
 
 % Projectile
 
+% Initial values
 xAcc = [0 0];
 yAcc = [acceleration(1)*sin(theta(1))-g, acceleration(2)*sin(theta(2)) - g];
 xVel = [velocity(1)*cos(theta(1)) velocity(2)*cos(theta(2))];
@@ -97,53 +101,52 @@ xPos = [x(1) x(2)];
 yPos = [y(1) y(2)];
 
 %calculating and drawing projectile
-for i = 1:400
+for i = 1:700
     clf;
     
+    %Air resistance;
     xAirres = [(0.5*(xVel(1)^2)*area(1)*air_constant(1))/mass(1),  (0.5*(xVel(2)^2)*area(2)*air_constant(2))/mass(2)];
-    yAirres = [(0.5*(yVel(1)^2)*area(1)*air_constant(1))/mass(1),  (0.5*(yVel(2)^2)*area(2)*air_constant(2))/mass(2)];
-    
-    xVel = [euler(tStep, xVel(1), xAcc(1)), euler(tStep, xVel(2), xAcc(2))]; 
-    yVel = [euler(tStep, yVel(1), yAcc(1)), euler(tStep, yVel(2), yAcc(2))];
-    
-    xPos = [euler(tStep, xPos(1), xVel(1)), euler(tStep, xPos(2), xVel(2))];
+    yAirres = [(0.5*(yVel(1)^2)*area(1)*air_constant(1))/mass(1),  (0.5*(yVel(2)^2)*area(2)*air_constant(2))/mass(2)];      
+    %Calculate Velocity
+    xVel = [euler(tStep, xVel(1), xAcc(1))- xAirres(1), euler(tStep, xVel(2), xAcc(2)) - xAirres(2)]; 
+    yVel = [euler(tStep, yVel(1), yAcc(1))- yAirres(1), euler(tStep, yVel(2), yAcc(2)) - yAirres(2)];
+    %Calculate Posistion
+    xPos = [euler(tStep, xPos(1), xVel(1)), euler(tStep, xPos(2), xVel(2))]; 
     yPos = [euler(tStep, yPos(1), yVel(1)), euler(tStep, yPos(2), yVel(2))];
-
-    %Calculating the bouncing angle
-    angle = [atan(yVel(1)/xVel(1)), atan(yVel(2)/xVel(2))];
     
-    %Collide 
-    if(sqrt((xPos(1) - xPos(2))^2 + (yPos(1) - yPos(2))^2) <= (radius(1)+radius(2))) 
-    %     Idée ett, funkar såååådär:
-%         xVel(1) = (xVel(1)*(mass(1) - mass(2)) + 2*mass(2)*xVel(2))/(mass(1)+mass(2));
-%         xVel(2) = (xVel(1)*(mass(2) - mass(1)) + 2*mass(1)*xVel(1))/(mass(1)+mass(2));
-%         
-%         yVel(1) = (yVel(1)*(mass(1) - mass(2)) + 2*mass(2)*yVel(2))/(mass(1)+mass(2));
-%         yVel(1) = (yVel(2)*(mass(2) - mass(1)) + 2*mass(1)*yVel(1))/(mass(1)+mass(2));
-
-    %     Idée två, funkar sååååådär:
-%           xVel(1) = (CRestitution*mass(2)*(xVel(2)-xVel(1)) + mass(1)*xVel(1) + mass(2)*xVel(2))/(mass(1) + mass(2));
-%           xVel(2) = (CRestitution*mass(1)*(xVel(1)-xVel(2)) + mass(2)*xVel(2) + mass(1)*xVel(1))/(mass(2) + mass(1));
-%             
-%           yVel(1) = (CRestitution*mass(2)*(yVel(2)-yVel(1)) + mass(1)*yVel(1) + mass(2)*yVel(2))/(mass(1) + mass(2));
-%           xVel(1) = (CRestitution*mass(1)*(yVel(1)-yVel(2)) + mass(2)*yVel(2) + mass(1)*yVel(1))/(mass(2) + mass(1));
+    %Collide    
+    if(sqrt((xPos(1) - xPos(2))^2 + (yPos(1) - yPos(2))^2) < (radius(1)+radius(2))) 
+        r2 = radius(1)+radius(2);
         
-    % Idée 3: http://geekswithblogs.net/robp/archive/2008/05/15/adding-collision-detection.aspx
-    % Måste säga att den funkar jävligt fint
-        xPos = tempxPos;
-        yPos = tempyPos;
-        xVel = tempxVel;
+        % Set VelVec to the old ones.
         yVel = tempyVel;
-    
-        x = [xPos(2) yPos(2) 0] - [xPos(1) yPos(1) 0];
+        xVel = tempxVel;
+        
+        % Calc ang
+        angleOne = atan2(yVel(1),xVel(1));
+        angleTwo = atan2(yVel(2),xVel(2));
+        
+        %Move the objects from eachother
+        move = 0.001;
+        while(sqrt((xPos(1) - xPos(2))^2 + (yPos(1) - yPos(2))^2) < r2)
+        xPos(1) = xPos(1) - (move * cos(angleOne));
+        yPos(1) = yPos(1) - (move * sin(angleOne));
+        
+        xPos(2) = xPos(2) - (move * cos(angleTwo));
+        yPos(2) = yPos(2) - (move * sin(angleTwo));
+        end
+        
+        %Find nor.plane
+        x = [xPos(2) yPos(2)] - [xPos(1) yPos(1)];
         x = x/norm(x);
-        v1 = [xVel(1) yVel(2) 0];
-        x1 = dot(x, v1);
+        
+        v1 = [xVel(1) yVel(1)];
+        x1 = dot(x, v1); 
         v1x = x*x1;
         v1y = v1 - v1x;
         
         x = -x;
-        v2 = [xVel(2) yVel(2) 0];
+        v2 = [xVel(2) yVel(2)];
         x2 = dot(x, v2);
         v2x = x*x2;
         v2y = v2-v2x;
@@ -171,10 +174,10 @@ for i = 1:400
     end
     
     %Hit Wall
-    if(xPos(1) < wSize(1)+wWidth || xPos(1) > wSize(2)-wWidth)
+    if(xPos(1)-radius(1) < wSize(1)+wWidth || xPos(1)+radius(1) > wSize(2)-wWidth)
         xVel(1) = -xVel(1);
     end
-    if(xPos(2) < wSize(1)+wWidth || xPos(2) > wSize(2)-wWidth)
+    if(xPos(2)-radius(2) < wSize(1)+wWidth || xPos(2)+radius(2) > wSize(2)-wWidth)
         xVel(2) = -xVel(2);
     end
      
@@ -185,7 +188,7 @@ for i = 1:400
     yp2=radius(2)*sin(circle_vec);
     
     patch(xPos(1)+xp,yPos(1)+yp, [1,0,0]);
-    patch(xPos(2)+xp2,yPos(2)+yp2, [1,0,0]);
+    patch(xPos(2)+xp2,yPos(2)+yp2, [0,1,0]);
 
     %Drawing the wall
     wallXData=[wSize(1), wSize(1)+wWidth, wSize(1)+wWidth, wSize(2)-wWidth, wSize(2)-wWidth, wSize(2), wSize(2), wSize(1)];
@@ -199,8 +202,9 @@ for i = 1:400
     
     tempxPos = xPos;
     tempyPos = yPos;
-    tempxVel = xVel;
     tempyVel = yVel;
+    tempxVel = xVel;
+    
     
     pause(1/100);
 end
